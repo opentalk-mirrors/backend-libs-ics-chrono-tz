59af4490fb5b4badab71e7726b09697666009894
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2023-01-26 13:39:15 +0100

    Add license information and `reuse` CI check

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+
 use chrono::{Datelike, NaiveDateTime};
 use core::panic;
 use parse_zoneinfo::line::{DaySpec, Line, LineParser, Weekday, Year};

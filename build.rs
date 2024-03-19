922b61ff5f9e00fe5c5e12bdc062e546d4edc3b0
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-03-19 10:36:20 +0100

    license: switch to MIT OR Apache-2.0 from EUPL-1.2
    Closes #3

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
 //
-// SPDX-License-Identifier: EUPL-1.2
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
 use chrono::{DateTime, Datelike};
 use parse_zoneinfo::line::{DaySpec, Line, LineParser, Weekday, Year};
